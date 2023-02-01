2725cf71884bbf86300582f7a03988aef443aa7e
kdan80 <kierandansey80@gmail.com>
2023-02-01 20:12:19 +0000

    Rewrite functions as ES6 arrow functions

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,43 @@
 import fs from 'fs'
 
-const ID3v2 = Buffer.from('494433', 'hex')
+type bytes = {
+    start: number
+    end: number
+}
 
-async function readFirstNBytes(path: fs.PathLike, n: number): Promise<Buffer> {
+// This function returns a specified number of bytes from a file
+// It takes in a file and a start & stop value as parameters
+// e.g passing in (song.mp3, {0, 2}) will return the first 3 bytes of song.mp3 
+// This function is called by other functions to look for magic numbers/file signatures
+// N.B. This function uses a readStream rather than reading the entire file into ram
+const get_n_bytes_from_file = async(path: string, { start, end }: bytes): Promise<Buffer> => {
+    
     const chunks = []
-    for await (const chunk of fs.createReadStream(path, { start: 0, end: n })) {
-      chunks.push(chunk)
+    for await (const chunk of fs.createReadStream(path, { start, end })) {
+        chunks.push(chunk)
     }
     
     return Buffer.concat(chunks)
 }
 
-try {
-    const magicNumber = await readFirstNBytes('/home/kd/Projects/Anoid/media/intro.mp3', 2)
-    const bufferComparison = Buffer.compare(magicNumber, ID3v2)
+
+// This function determines if a file is truly an mp3 with ID3v2 metadata
+// It looks for the magic number/file signature <49 44 33> at the start of the file
+const file_is_ID3v2_mp3 = async(path: string): Promise<boolean> => {
     
-    if (bufferComparison === 0) console.log('Valid mp3 file')
-} catch(err: any) {
-    console.log('There was an error')
+    try {
+        const magicNumber = await get_n_bytes_from_file(path, {start: 0, end: 2})
+        const ID3v2 = Buffer.from('494433', 'hex')
+        const bufferComparison = Buffer.compare(magicNumber, ID3v2)
+        
+        // 0 means eqauality with Buffer.compare()
+        if (bufferComparison === 0) return true;
+        return false
+    } catch (err) {
+        return false
+    }
 }
 
+const isMp3 = await file_is_ID3v2_mp3('/home/kd/Projects/Anoid/media/intro.mp3');
+
+console.log('intro.mp3 is a valid mp3 =', isMp3)
