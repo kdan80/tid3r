d150584ae16469559ebaa7841edabb5ff732d209
kdan80 <kierandansey80@gmail.com>
2023-02-02 20:16:06 +0000

    Added function to calculate ID3 data length

--- src/index.ts
+++ src/index.ts
@@ -33,9 +33,14 @@ const get_n_bytes_from_read_stream = (range: Range) => {
     }
 }
 
-const get_ID3v1_magic_number = get_n_bytes_from_read_stream({start: 0, end: 1})
-const get_ID3v2_magic_number = get_n_bytes_from_read_stream({start: 0, end: 2})
-const get_flac_magic_number = get_n_bytes_from_read_stream({start: 0, end: 3})
+const get_ID3_header = get_n_bytes_from_read_stream({start: 0, end: 9})
+
+const calculate_ID3_data_length_in_bytes = (ID3_length_bytes: Buffer) => {
+    return  (ID3_length_bytes.readUInt8(0) << 21) 
+            |   (ID3_length_bytes.readUInt8(1) << 14) 
+            |   (ID3_length_bytes.readUInt8(2) << 7) 
+            |   (ID3_length_bytes.readUInt8(3))
+}
 
 const compare_hexstring_to_magic_number = (hexstring_from_file: string): (magic_number: string) => boolean => {
     return (magic_number: string) => {
@@ -43,15 +48,17 @@ const compare_hexstring_to_magic_number = (hexstring_from_file: string): (magic_
     }
 }
 
+// The following functions are used to validate whether a hexstring is a magic number
+const compare_hexstring_to_ID3v2 = compare_hexstring_to_magic_number(magic_number_ID3v2)
+const compare_hexstring_to_ID3v1 = compare_hexstring_to_magic_number(magic_number_ID3v1)
+const compare_hexstring_to_flac = compare_hexstring_to_magic_number(magic_number_flac)
+
+
 const check_audio_format_is_supported = (audio_file_path: string): boolean => {
     const file_extension = path.extname(audio_file_path).toLowerCase()
     return supported_audio_formats.includes(file_extension)
 }
 
-// The following functions are used to validate whether a hexstring is a magic number
-const compare_hexstring_to_ID3v2 = compare_hexstring_to_magic_number(magic_number_ID3v2)
-const compare_hexstring_to_ID3v1 = compare_hexstring_to_magic_number(magic_number_ID3v1)
-const compare_hexstring_to_flac = compare_hexstring_to_magic_number(magic_number_flac)
 
 
 
