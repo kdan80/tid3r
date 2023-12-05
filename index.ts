7116c81e03f26a1af7dc58d89a9c66fef7e5d8bb
kdan80 <kierandansey80@gmail.com>
2023-12-05 19:44:16 +0000

    test createReadStream

--- index.ts
+++ index.ts
@@ -1,1 +1,16 @@
-console.log("hello world")
+
+import fs from 'fs'
+
+const file = "/home/kd/Projects/tid3r/media/cwm.mp3"
+
+const stream = fs.createReadStream(file, {encoding: 'hex'})
+
+stream.on('data', function (chunk) {
+   
+    if (chunk.slice(0,6) === "494433") {
+        console.log(chunk.slice(0,10))
+    }	    
+   
+}); 
+
+//console.log("file: ", file)
