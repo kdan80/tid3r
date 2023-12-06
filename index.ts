7eb0fe20c61db87d4965e77c9aa080886212ea8c
kdan80 <kierandansey80@gmail.com>
2023-12-06 18:20:00 +0000

    testing stream.readable

--- index.ts
+++ index.ts
@@ -5,11 +5,16 @@ const file = "/home/kd/Projects/tid3r/media/cwm.mp3"
 
 const stream = fs.createReadStream(file, {encoding: 'hex'})
 
-stream.on('data', function (chunk) {
-   
+let i = 0
+stream.on('readable', () => {
+    
+    console.log('ind: ', i)	
+    let chunk = stream.read()
     if (chunk.slice(0,6) === "494433") {
         console.log(chunk.slice(0,10))
     }	    
+
+    i++
    
 }); 
 
