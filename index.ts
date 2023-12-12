e8bc4e9a89f5164391ce2c939950615ffb95bf4a
kdan80 <kierandansey80@gmail.com>
2023-12-12 18:56:36 +0000

    parse tag header

--- index.ts
+++ index.ts
@@ -1,20 +1,46 @@
 
 import fs from 'fs'
 
+interface TagHeader {
+	major_version?: string
+	minor_version?: string
+	unsynchronisation?: boolean
+	extended_header?: boolean
+	experimental?: boolean
+	footer?: boolean
+	flags?: any
+	size?: any
+}
+
 const file = "/home/kd/Projects/tid3r/media/cwm.mp3"
 
 const stream = fs.createReadStream(file, {encoding: 'hex'})
 
 let i = 0
-stream.on('readable', () => {
-    
-    console.log('ind: ', i)	
-    let chunk = stream.read()
-    if (chunk.slice(0,6) === "494433") {
-        console.log(chunk.slice(0,10))
-    }	    
 
-    i++
+let tag_header: TagHeader = {}
+
+const hex2bin = (hex: string) => {
+	return (parseInt(hex, 16).toString(2)).padStart(8, '0')
+}
+
+stream.on('readable', () => {
+
+    let chunk = stream.read()
+	if (chunk === null) return
+    if (chunk.slice(0,6) !== "494433" && i === 0) return 
+
+
+  	if ( i === 0 ){
+		tag_header.major_version = chunk.slice(6,8)
+		tag_header.minor_version = chunk.slice(9,11)
+		const flags = hex2bin(chunk.slice(12,13))
+		tag_header.flags = flags
+		console.log('head: ', tag_header)
+
+	}
+
+	i++
    
 }); 
 
